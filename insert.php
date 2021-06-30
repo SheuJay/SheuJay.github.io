e3f96c812f748f7c1542e18a4ec4870075887daf
SheuJay <86692622+SheuJay@users.noreply.github.com>
2021-06-30 10:06:55 -0700

    tes

---
+++ insert.php
@@ -0,0 +1,15 @@
+<?php
+   
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "sheujaygwah8@gmail.com";
+$subject = "Mail From Superprint Media website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@Superprint Media Website.com" . "\r\n" .
+"CC: sheujaygwah8@gmail.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+header("Location:thankyou.html");
+?>
